6755939b0036f4daa6775c66809af7c80770477b
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-05-28 12:51:57 -0700

    update fspath

--- gothumb.go
+++ gothumb.go
@@ -71,7 +71,7 @@ func CreateMoviesThumbnail(p string) (ThumbINFO ThumbInFo) {
 	// thumbpathone := "./static/" + basepath
 	var BP string = "/" + basepath
 	var thumbpathtwo string = MSA + ":" + MSP + MTPP + BP
-	// var thumbpathone string = "/static/" + basepath
+	var thumbpathone string = "/root/static/" + basepath
 
 
 
@@ -80,7 +80,7 @@ func CreateMoviesThumbnail(p string) (ThumbINFO ThumbInFo) {
 	ThumbINFO.BasePath = basepath
 	ThumbINFO.DirPATH = dirpath
 	ThumbINFO.HTTPThumbPath = thumbpathtwo
-	ThumbINFO.ThumbPath = thumbpathtwo
+	ThumbINFO.ThumbPath = thumbpathone
 	ThumbINFO.ThumbID = UUID()
 
 	if ext == ".txt" {
